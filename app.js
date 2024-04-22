ce42ad66a8d87f754cbc0a32550c3cbd3295ae44
shahriar021 <shahriarc19@gmail.com>
2024-04-23 01:22:05 +0600

    assignment 1

---
+++ app.js
@@ -0,0 +1,20 @@
+// const heading = React.createElement(
+//   "h2",
+//   { className: "reactName" },
+//   "this is by react from app."
+// );
+
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("p", {}, "this is a p tag"),
+    React.createElement("p", {}, "this is a p tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("p", {}, "this is a p tag"),
+    React.createElement("p", {}, "this is a p tag"),
+  ]),
+]);
+
+const rDom = ReactDOM.createRoot(document.getElementById("id"));
+
+rDom.render(heading);
